01921d857a22adc4835d19fc23220efdbc8bd599
Jens-Uwe Mager <jum@anubis.han.de>
2019-09-01 14:39:01 +0200

    Refactor the main processing loop so we can easier wrap the error to find out which file produced an error.

--- htempl.go
+++ htempl.go
@@ -11,8 +11,8 @@ import (
 	"os"
 	"path/filepath"
 
-    "gopkg.in/yaml.v2"
-    "github.com/russross/blackfriday"
+	"github.com/russross/blackfriday"
+	"gopkg.in/yaml.v2"
 )
 
 const (
@@ -27,96 +27,104 @@ var (
 func main() {
 	flag.Parse()
 	for _, fname := range flag.Args() {
-        //fmt.Printf("working on %s\n", fname)
-        in, err := os.Open(fname)
-        if err != nil {
-            panic(err)
-        }
-		header, body, err := splitHeader(in)
+		//fmt.Printf("working on %s\n", fname)
+		err := processFile(fname)
 		if err != nil {
+			err = fmt.Errorf("%v: %w", fname, err)
 			panic(err)
 		}
-		vars := make(map[string]interface{})
-		templ := template.New("htempl").Funcs(template.FuncMap{
-            "md2html": func(value string) template.HTML {
-                return template.HTML(blackfriday.MarkdownCommon([]byte(value)))
-            },
-            "htmlattr": func(value string) template.HTMLAttr {
-                return template.HTMLAttr(value)
-            },
-        })
-		if header != nil {
-			dec := yaml.NewDecoder(header)
-			err = dec.Decode(&vars)
-			if err != nil {
-				panic(err)
-			}
-        }
-        var includeFiles []string
-		fn, ok := vars["include"]
-		if ok {
+	}
+}
+
+func processFile(fname string) error {
+	in, err := os.Open(fname)
+	if err != nil {
+		panic(err)
+	}
+	defer in.Close()
+	header, body, err := splitHeader(in)
+	if err != nil {
+		return err
+	}
+	vars := make(map[string]interface{})
+	templ := template.New("htempl").Funcs(template.FuncMap{
+		"md2html": func(value string) template.HTML {
+			return template.HTML(blackfriday.MarkdownCommon([]byte(value)))
+		},
+		"htmlattr": func(value string) template.HTMLAttr {
+			return template.HTMLAttr(value)
+		},
+	})
+	if header != nil {
+		dec := yaml.NewDecoder(header)
+		err = dec.Decode(&vars)
+		if err != nil {
+			return err
+		}
+	}
+	var includeFiles []string
+	fn, ok := vars["include"]
+	if ok {
+		includeFiles = append(includeFiles, fn.(string))
+	}
+	fnArray, ok := vars["includes"]
+	if ok {
+		for _, fn := range fnArray.([]interface{}) {
 			includeFiles = append(includeFiles, fn.(string))
 		}
-		fnArray, ok := vars["includes"]
-		if ok {
-			for _, fn := range fnArray.([]interface{}) {
-				includeFiles = append(includeFiles, fn.(string))
+	}
+	if len(includeFiles) > 0 {
+		for _, fn := range includeFiles {
+			f, err := os.Open(fn)
+			if err != nil {
+				return err
 			}
-        }
-        if len(includeFiles) > 0 {
-            for _, fn := range includeFiles {
-                f, err := os.Open(fn)
-                if err != nil {
-                    panic(err)
-                }
-                dec := yaml.NewDecoder(f)
-                err = dec.Decode(&vars)
-                if err != nil {
-                    panic(err)
-                }
-                f.Close()
-            }
-        }
-		//fmt.Printf("Header vars: %#v\n", vars)
-        var templateFiles []string
-		fn, ok = vars["template"]
-		if ok {
+			dec := yaml.NewDecoder(f)
+			err = dec.Decode(&vars)
+			if err != nil {
+				return err
+			}
+			f.Close()
+		}
+	}
+	//fmt.Printf("Header vars: %#v\n", vars)
+	var templateFiles []string
+	fn, ok = vars["template"]
+	if ok {
+		templateFiles = append(templateFiles, fn.(string))
+	}
+	fnArray, ok = vars["templates"]
+	if ok {
+		for _, fn := range fnArray.([]interface{}) {
 			templateFiles = append(templateFiles, fn.(string))
 		}
-		fnArray, ok = vars["templates"]
-		if ok {
-			for _, fn := range fnArray.([]interface{}) {
-				templateFiles = append(templateFiles, fn.(string))
-			}
-        }
-        if len(templateFiles) > 0 {
-		    templ, err = templ.ParseFiles(templateFiles...)
-		    if err != nil {
-			    panic(err)
-            }
-        }
-		// Why is there no function to parse from an io.Reader?
-		main, err := ioutil.ReadAll(body)
-		if err != nil {
-			panic(err)
-		}
-		templ, err = templ.Parse(string(main))
-		if err != nil {
-			panic(err)
-        }
-        in.Close()
-		ext := filepath.Ext(fname)
-		dname := filepath.Join(*dest, fname[0:len(fname)-len(ext)]+".html")
-        out, err := os.Create(dname)
-        if err != nil {
-            panic(err)
-        }
-        err = templ.Execute(out, vars)
-        if err != nil {
-            panic(err)
-        }
-        out.Close()
 	}
+	if len(templateFiles) > 0 {
+		templ, err = templ.ParseFiles(templateFiles...)
+		if err != nil {
+			return err
+		}
+	}
+	// Why is there no function to parse from an io.Reader?
+	main, err := ioutil.ReadAll(body)
+	if err != nil {
+		return err
+	}
+	templ, err = templ.Parse(string(main))
+	if err != nil {
+		return err
+	}
+	ext := filepath.Ext(fname)
+	dname := filepath.Join(*dest, fname[0:len(fname)-len(ext)]+".html")
+	out, err := os.Create(dname)
+	if err != nil {
+		return err
+	}
+	err = templ.Execute(out, vars)
+	if err != nil {
+		return err
+	}
+	return out.Close()
 }
 
 func splitHeader(in io.Reader) (header, body io.Reader, err error) {
