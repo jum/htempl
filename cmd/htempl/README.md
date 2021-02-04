d1f6d116992ee96c3c01f4c69c8c5a588e7112d9
Jens-Uwe Mager <jum@anubis.han.de>
2021-02-04 12:44:25 +0100

    Add arguments for source file name suffix, add an option to traverse a complete directory tree.

--- cmd/htempl/README.md
+++ cmd/htempl/README.md
@@ -1,10 +1,17 @@
 # htempl
 
 A simple command to generate HTML files using go templates combined with
-data from YAML files.
+data from YAML files. Please note that all references to files are relative
+to the working directory of the command.
 
 ```sh
-htempl -dest destdir -suffix html *.htempl
+htempl -dest destdir -destsuffix html *.htempl
+```
+
+You can also process a tree of files:
+
+```sh
+htempl -dest destdir -src srcdir -destsuffix html -srcsuffix htempl
 ```
 
 Please see the README.md of the library for details of the file format.
--- cmd/htempl/main.go
+++ cmd/htempl/main.go
@@ -3,20 +3,53 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/fs"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/jum/htempl"
 )
 
 var (
 	dest    = flag.String("dest", ".", "Destination directory")
-	suffix  = flag.String("suffix", "html", "Default suffix for generated files")
+	src     = flag.String("src", "", "source directory to walk directory")
+	ssuffix = flag.String("srcsuffix", "htempl", "Default suffix for source files")
+	dsuffix = flag.String("destsuffix", "html", "Default suffix for generated files")
 	verbose = flag.Bool("verbose", false, "verbose debugging output")
 )
 
 func main() {
 	flag.Parse()
+	// walk the source directory if given
+	if len(*src) > 0 {
+		err := filepath.Walk(*src, func(path string, info fs.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if *verbose {
+				fmt.Printf("considering %s\n", path)
+			}
+			if filepath.Ext(path) == "."+*ssuffix {
+				if *verbose {
+					fmt.Printf("working on %s\n", path)
+				}
+				err := processFile(path)
+				if err != nil {
+					err = fmt.Errorf("%v: %w", path, err)
+					fmt.Fprintf(os.Stderr, "htempl: %v\n", err)
+					os.Exit(1)
+				}
+			}
+			return nil
+		})
+		if err != nil {
+			err = fmt.Errorf("%v: %w", *src, err)
+			fmt.Fprintf(os.Stderr, "htempl: %v\n", err)
+			os.Exit(1)
+		}
+	}
+	// process the explicitely givenvi command line args
 	for _, fname := range flag.Args() {
 		if *verbose {
 			fmt.Printf("working on %s\n", fname)
@@ -36,7 +69,12 @@ func processFile(fname string) error {
 		return err
 	}
 	ext := filepath.Ext(fname)
-	dname := filepath.Join(*dest, fname[0:len(fname)-len(ext)]+"."+*suffix)
+	if len(*src) > 0 {
+		if strings.HasPrefix(fname, *src) {
+			fname = fname[len(*src):]
+		}
+	}
+	dname := filepath.Join(*dest, fname[0:len(fname)-len(ext)]+"."+*dsuffix)
 	out, err := os.Create(dname)
 	if err != nil {
 		return err
